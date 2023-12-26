1ba8a723827c4c3b234550cabbf5a3456949cdf8
vladimirvalk <i5186812@gmail.com>
2023-12-26 10:28:15 +0000

    Remove unused code

--- src/main/java/ee/testrest/mobile/ios/saucelabs/screens/cart/CartScreen.java
+++ src/main/java/ee/testrest/mobile/ios/saucelabs/screens/cart/CartScreen.java
@@ -17,25 +17,12 @@ public class CartScreen extends CartItem {
         return this;
     }
 
-    //    private final SelenideElement testItem = element(byName("test-Item"));
-//    private final SelenideElement productQuantity = testItem.find("[name='test-Amount'] [type='XCUIElementTypeStaticText']");
-//    private final SelenideElement productDescription = testItem.find("[name='test-Description'] [type='XCUIElementTypeStaticText']");
-//    private final SelenideElement productPrice = testItem.find("[name='test-Price'] [type='XCUIElementTypeStaticText']");
     private final SelenideElement checkoutButton = element(byName("test-CHECKOUT"));
     private final SelenideElement continueShoppingButton = element(byName("test-CONTINUE SHOPPING"));
-
-//    @Step("Verify product details")
-//    public CartScreen verifyProductDetails() {
-//        productQuantity.shouldNotBe(empty);
-//        productDescription.shouldNotBe(empty);
-//        productPrice.shouldNotBe(empty);
-//        return this;
-//    }
 
     @Step("Proceed to checkout screen")
     public CheckoutInformationScreen proceedToCheckout() {
         checkoutButton.click();
         return screen(new CheckoutInformationScreen().load());
     }
-
 }
